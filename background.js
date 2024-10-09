1dd22cda1806f5f889d6b028ed3edb4dbbb5b79f
Scott Wegley <scbrwegley@protonmail.ch>
2024-10-08 21:00:23 -0700

    integrated code from sidebar template

--- background.js
+++ background.js
@@ -1,1 +1,10 @@
-console.log("Pokedle Assistant Loaded");
+
+'use strict';
+
+console.log("Pokedle Assistant - Background Worker Loaded");
+
+let registered = null;
+
+
+
+browser.runtime.onMessage.addListener(registerScript);
